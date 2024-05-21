3dc3cc560eaaff1e480f41dd01bdd7321154eb27
Robbie Chapman <robbie290693@gmail.com>
2024-05-21 16:36:37 +0100

    added insertUserGame function (updated)

--- server/db.tsx
+++ server/db.tsx
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js")
 
-const supabaseUrl = "https://ubmzmglsaigdsxuhgkmv.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibXptZ2xzYWlnZHN4dWhna212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxOTMzMTUsImV4cCI6MjAzMTc2OTMxNX0.dAxuKuUP5yTDrP5pQK-FO--SSpyKE9j1GWKd6S6elDU"
+const supabaseUrl = "URL"
+const supabaseKey = "key"
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
