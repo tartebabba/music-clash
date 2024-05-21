ff69aff3b8d344a9cfa39bc87894c7580929aa2f
Robbie Chapman <robbie290693@gmail.com>
2024-05-21 12:19:25 +0100

    DS1 tickets - .0,.1,.2 complete

--- server/db.tsx
+++ server/db.tsx
@@ -1,9 +1,85 @@
-import { createClient } from "@supabase/supabase-js";
+const { createClient } = require("@supabase/supabase-js")
 
-const supabase = createClient("https://ubmzmglsaigdsxuhgkmv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibXptZ2xzYWlnZHN4dWhna212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxOTMzMTUsImV4cCI6MjAzMTc2OTMxNX0.dAxuKuUP5yTDrP5pQK-FO--SSpyKE9j1GWKd6S6elDU")
-async function getGames() {
-  const { data } = await supabase.from("games").select()
+const supabaseUrl = "URL"
+const supabaseKey = "KEY"
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function getGames() {
+  const { data } = await supabase
+  .from("games")
+  .select()
   console.log(data)
 }
 
-export default getGames
+export async function getGameDetails() {
+    const { data } = await supabase
+    .from("game_details")
+    .select()
+    console.log(data)
+  }
+
+
+  interface Game {
+    date: string
+  }
+  export async function postGame(postObject:Game) {
+    const {data} = await supabase
+    .from('games')
+    .insert(postObject)
+    .select()
+    console.log(data![0].id); 
+  }
+
+  interface GameDetails {
+    artist: string
+    song_1: string
+    song_2: string
+    song_3: string
+    song_4: string
+  }
+
+  export async function postMultipleGameDetails(postObject:GameDetails[]){
+    const data = await supabase
+    .from('game_details')
+    .insert(postObject)
+    .select()
+    console.log(data); 
+  }
+
+  export async function createGame(postObject:GameDetails[]) {
+    try{
+        let currentDate = new Date().toJSON();
+        const {data, error: gameError} = await supabase
+        .from('games')
+        .insert({date: currentDate})
+        .select()
+        if (gameError) {
+            console.log(gameError);
+            throw new Error(`Error inserting game record: ${gameError.message}`);
+        }
+        const gameId = data![0].id
+        const mappedPostObject = postObject.map((row) => {
+            return {game_id:gameId, ...row}
+        })
+        
+        const {error: detailsError} = await supabase
+        .from('game_details')
+        .insert(mappedPostObject)
+        .select()
+        if (detailsError) {
+            console.log(detailsError);
+            throw new Error(`Error inserting game details: ${detailsError.message}`);
+        }
+    }
+    catch(error) {
+        console.error('Error in createGame:', error);
+        throw error;
+    }
+  }
+
+  // make create user function
+  // make insert user_game function
+  // get songs by artist function
+  // table top 100 artists top 10 (artist_songs_combinations)
+  // prevent duplicate artist/game data 
