637b877b56feb3ec1f2bfb5cc1613369532c3c6c
Ted Smiles <ted@smilesfamily.co.uk>
2024-05-24 15:07:14 +0100

    adding file for multiplayer

---
+++ frontend/src/components/Multiplayer.tsx
@@ -0,0 +1,507 @@
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
+
+import io, { Socket } from "socket.io-client";
+
+const socket: Socket = io("http://localhost:4000");
+
+export default function Multiplayer() {
+  const gameItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+  const groups = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 14, 11, 16],
+    [13, 10, 15, 12],
+  ];
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    centered: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    cardContainer: {
+      width: 100,
+      height: 100,
+      margin: 5,
+    },
+    card: {
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      backfaceVisibility: "hidden",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    cardButton: {
+      backgroundColor: "lightblue",
+      width: "100%",
+      height: "100%",
+      position: "relative",
+    },
+    waitingCard: {
+      backgroundColor: "darkgrey",
+      width: "100%",
+      height: "100%",
+      position: "relative",
+    },
+    selectedCard: {
+      backgroundColor: "lightgreen",
+      width: "100%",
+      height: "100%",
+      position: "relative",
+    },
+    foundCard: {
+      width: "100%",
+      height: "100%",
+      position: "relative",
+    },
+    button: {
+      backgroundColor: "lightgrey",
+      padding: 10,
+      margin: 10,
+    },
+    disableButton: {
+      backgroundColor: "darkgrey",
+      padding: 10,
+      margin: 10,
+    },
+    input: {
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 10,
+      margin: 10,
+      borderColor: "black",
+      borderWidth: 2,
+    },
+    row: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    boardContainer: {
+      flex: 1,
+      alignItems: "center",
+      margin: 40,
+    },
+  });
+
+  type OpponentEvents = {
+    lives: number;
+    found_groups: number[];
+    events: string[];
+  };
+
+  type Score = {
+    player1: number;
+    player2: number;
+  };
+
+  const [selected, setSelected] = useState<number[]>([]);
+  const [foundGroups, setFoundGroups] = useState<number[]>([]);
+  const [guessResult, setGuessResult] = useState<string>("");
+  const [lives, setLives] = useState<number>(2);
+  const [room, setRoom] = useState<string>("");
+  const [user, setUser] = useState<string>("user1");
+  const [opponentEvents, setOpponentEvents] = useState<OpponentEvents>({
+    lives: 2,
+    found_groups: [],
+    events: [],
+  });
+  const [score, setScore] = useState<Score>({ player1: 0, player2: 0 });
+  const [rematchRequested, setRematchRequested] = useState<boolean>(false);
+  const [opponentRematchRequested, setOpponentRematchRequested] =
+    useState<boolean>(false);
+  const [availableRooms, setAvailableRooms] = useState<string[]>([]);
+  const [selectedRoom, setSelectedRoom] = useState<string>("");
+  const [isFull, setIsFull] = useState<boolean>(false);
+  const [isAlreadyFull, setIsAlreadyFull] = useState<boolean>(false);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+  const [showResults, setShowResults] = useState<boolean>(false);
+  const [opponentDisconnected, setOpponentDisconnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    socket.emit("getAvailableRooms");
+  }, []);
+
+  useEffect(() => {
+    resetGameState();
+
+    socket.on("availableRooms", ({ rooms }) => {
+      setAvailableRooms(Object.keys(rooms));
+    });
+
+    socket.on("roomFull", () => {
+      setIsAlreadyFull(true);
+      setRoom("");
+      setSelectedRoom("");
+    });
+
+    socket.on("roomReady", () => {
+      setOpponentDisconnected(false);
+      setIsFull(true);
+    });
+
+    socket.on("opponentDisconnected", () => {
+      setIsFull(false);
+      setOpponentDisconnected(true);
+      resetGameState();
+    });
+
+    socket.on("groupFound", ({ group }) => {
+      setOpponentEvents((prevEvents) => ({
+        ...prevEvents,
+        found_groups: [...prevEvents.found_groups, ...group],
+        events: [...prevEvents.events, `Group found: ${group.join(", ")}`],
+      }));
+    });
+    socket.on("incorrectGuess", () => {
+      setOpponentEvents((prevEvents) => ({
+        ...prevEvents,
+        lives: prevEvents.lives - 1,
+        events: [...prevEvents.events, "Life lost"],
+      }));
+    });
+
+    socket.on("checkScores", (scores) => {
+      console.log(scores);
+      console.log(scores.player2);
+      setScore({
+        player1: scores.player1,
+        player2: scores.player2,
+      });
+    });
+
+    socket.on("rematchRequest", () => {
+      setOpponentRematchRequested(true);
+    });
+
+    socket.on("confirmRematch", () => {
+      resetGameState();
+      setRematchRequested(false);
+      setOpponentRematchRequested(false);
+    });
+
+    return () => {
+      socket.off("availableRooms");
+      socket.off("roomFull");
+      socket.off("roomReady");
+      socket.off("opponentDisconnected");
+      socket.off("groupFound");
+      socket.off("incorrectGuess");
+      socket.off("checkScores");
+      socket.off("rematchRequest");
+      socket.off("confirmRematch");
+    };
+  }, [room]);
+
+  function handleClick(item: number) {
+    if (selected.includes(item)) {
+      setSelected(
+        selected.filter((elem) => {
+          return elem !== item;
+        })
+      );
+    } else if (selected.length < 4) {
+      setSelected([...selected, item]);
+    }
+  }
+
+  function handleSubmit() {
+    const guess = selected.sort().join("");
+    setGuessResult("incorrect");
+    let correct = false;
+
+    for (let i = 0; i < groups.length; i++) {
+      if (groups[i].sort().join("") === guess) {
+        setFoundGroups([...foundGroups, ...selected]);
+        setGuessResult("correct");
+        correct = true;
+
+        socket.emit("groupFound", room, user, selected);
+
+        if (foundGroups.length === 12) {
+          setGuessResult("winner");
+        }
+      }
+    }
+    setSelected([]);
+
+    if (correct === false) {
+      setLives(lives - 1);
+      socket.emit("incorrectGuess", room, user, selected);
+    }
+  }
+
+  function handlePlayAgain() {
+    setRematchRequested(true);
+    socket.emit("rematchRequest", { room });
+  }
+
+  function confirmRematch() {
+    socket.emit("confirmRematch", { room });
+    resetGameState();
+    setRematchRequested(false);
+    setOpponentRematchRequested(false);
+  }
+
+  function resetGameState() {
+    setFoundGroups([]);
+    setGuessResult("");
+    setLives(2);
+    setGameOver(false);
+    setShowResults(false);
+    setOpponentEvents({
+      lives: 2,
+      found_groups: [],
+      events: [],
+    });
+  }
+
+  function getBackgroundColor(item: number) {
+    const colours = ["red", "blue", "purple", "green"];
+    const index = foundGroups.indexOf(item);
+    const groupIndex = Math.floor(index / 4);
+    return colours[groupIndex];
+  }
+
+  function getOpponentBackgroundColor(index: number) {
+    const colours = ["red", "blue", "purple", "green"];
+    const groupIndex = Math.floor(index / 4);
+    return colours[groupIndex];
+  }
+
+  const handleCreateRoom = () => {
+    const newRoomName = `${user}'s Room`;
+    setRoom(newRoomName);
+    socket.emit("createRoom", user, newRoomName);
+  };
+
+  const handleJoinRoom = () => {
+    if (selectedRoom) {
+      setRoom(selectedRoom);
+      socket.emit("joinRoom", user, selectedRoom);
+    }
+  };
+
+  const handleLeaveRoom = () => {
+    socket.emit("leaveRoom", user, room);
+    setRoom("");
+    setSelectedRoom("");
+  };
+
+  useEffect(() => {
+    if (
+      (lives === 0 ||
+        foundGroups.length === 16 ||
+        opponentEvents.lives === 0 ||
+        opponentEvents.found_groups.length === 16) &&
+      gameOver === false
+    ) {
+      setShowResults(true);
+    }
+
+    if ((lives === 0 || foundGroups.length === 16) && gameOver === false) {
+      setGameOver(true);
+    }
+  }, [lives, foundGroups, opponentEvents, gameOver]);
+
+  useEffect(() => {
+    if (gameOver) {
+      console.log("Test");
+      socket.emit("gameOver", user, room, lives);
+    }
+  }, [gameOver]);
+
+  useEffect(() => {
+    console.log("Test1");
+    if (opponentDisconnected === true) {
+      alert("Opponent has disconnected");
+      setTimeout(function () {
+        console.log("Test2");
+        setOpponentDisconnected(false);
+      }, 10000);
+    }
+  }, [opponentDisconnected]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.centered}>
+        <TextInput
+          style={styles.input}
+          placeholder="User..."
+          value={user}
+          onChangeText={setUser}
+        />
+        {room === "" ? (
+          <>
+            <Pressable style={styles.button} onPress={handleCreateRoom}>
+              <Text>Create New Room</Text>
+            </Pressable>
+
+            {availableRooms.length === 0 ? null : (
+              <>
+                <Picker
+                  selectedValue={selectedRoom}
+                  onValueChange={(itemValue: string) => setSelectedRoom(itemValue)}
+                  style={styles.input}
+                >
+                  <Picker.Item label="Select a room..." value="" />
+                  {availableRooms.map((room) => (
+                    <Picker.Item key={room} label={room} value={room} />
+                  ))}
+                </Picker>
+                <Pressable style={styles.button} onPress={handleJoinRoom}>
+                  <Text>Join Room</Text>
+                </Pressable>
+                {isAlreadyFull ? <Text>This room is full</Text> : null}
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <Text>{`Room: ${room}`}</Text>
+            <Pressable style={styles.button} onPress={handleLeaveRoom}>
+              <Text>Leave Room</Text>
+            </Pressable>
+          </>
+        )}
+
+        <Text>Welcome to Music Clash</Text>
+        {opponentDisconnected ? <Text>Opponent Disconnected</Text> : null}
+        <View style={styles.row}>
+          <View style={styles.boardContainer}>
+            <Text>You have {lives} lives remaining</Text>
+            <FlatList
+              data={gameItems}
+              numColumns={4}
+              renderItem={({ item }) => (
+                <View style={styles.cardContainer}>
+                  <Pressable
+                    onPress={() => handleClick(item)}
+                    style={
+                      foundGroups.includes(item)
+                        ? [
+                            styles.foundCard,
+                            {
+                              backgroundColor: getBackgroundColor(item),
+                            },
+                          ]
+                        : selected.includes(item)
+                        ? styles.selectedCard
+                        : styles.cardButton
+                    }
+                    disabled={
+                      foundGroups.includes(item) ||
+                      lives === 0 ||
+                      opponentEvents.found_groups.length === 16 ||
+                      !isFull
+                    }
+                  >
+                    <View style={styles.card}>
+                      <Text>{item}</Text>
+                    </View>
+                  </Pressable>
+                </View>
+              )}
+              keyExtractor={(item) => item.toString()}
+            />
+            <Pressable
+              disabled={selected.length !== 4}
+              style={
+                selected.length !== 4 ? styles.disableButton : styles.button
+              }
+              onPress={handleSubmit}
+            >
+              <Text>Submit</Text>
+            </Pressable>
+            {guessResult !== "" ? (
+              guessResult === "correct" || guessResult === "winner" ? (
+                <Text>Correct</Text>
+              ) : (
+                <Text>Try again</Text>
+              )
+            ) : null}
+            {guessResult === "winner" || opponentEvents.lives === 0 ? (
+              <>
+                <Text>Winner</Text>{" "}
+                <Pressable style={styles.button} onPress={handlePlayAgain}>
+                  <Text>Play again</Text>
+                </Pressable>
+              </>
+            ) : null}
+            {lives === 0 || opponentEvents.found_groups.length === 16 ? (
+              <>
+                <Text>You lose</Text> <Text></Text>
+                <Pressable style={styles.button} onPress={handlePlayAgain}>
+                  <Text>Play again</Text>
+                </Pressable>
+              </>
+            ) : null}
+            {showResults ? (
+              <Text>
+                Player1 {score.player1} wins : player2: {score.player2} wins
+              </Text>
+            ) : null}
+          </View>
+          <View style={styles.boardContainer}>
+            {!isFull ? (
+              <Text>Waiting for opponent</Text>
+            ) : (
+              <Text>Opponent has {opponentEvents.lives} lives remaining</Text>
+            )}
+            <FlatList
+              data={gameItems}
+              numColumns={4}
+              renderItem={({ item, index }) => (
+                <View style={styles.cardContainer}>
+                  <Pressable
+                    style={
+                      item <= opponentEvents.found_groups.length
+                        ? [
+                            styles.foundCard,
+                            {
+                              backgroundColor:
+                                getOpponentBackgroundColor(index),
+                            },
+                          ]
+                        : !isFull
+                        ? styles.waitingCard
+                        : styles.cardButton
+                    }
+                    disabled={true}
+                  >
+                    <View style={styles.card}>
+                      <Text></Text>
+                    </View>
+                  </Pressable>
+                </View>
+              )}
+              keyExtractor={(item) => item.toString()}
+            />
+          </View>
+        </View>
+        {rematchRequested && !opponentRematchRequested ? (
+          <Text>Waiting for opponent to confirm rematch...</Text>
+        ) : opponentRematchRequested && !rematchRequested ? (
+          <Pressable style={styles.button} onPress={confirmRematch}>
+            <Text>Confirm rematch</Text>
+          </Pressable>
+        ) : null}
+      </View>
+    </View>
+  );
+}
---
+++ server/index.ts
@@ -0,0 +1,158 @@
+import express from "express";
+import http from "http";
+import { Server as socketIo, Socket } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIo(server, {
+  cors: {
+    origin: "http://localhost:8081",
+    methods: ["GET", "POST"],
+  },
+});
+
+const port = 4000;
+
+type Room  = {
+    players: string[];
+    rematchRequests: number;
+    score: {
+      player1: number;
+      player2: number;
+    };
+  }
+  
+  const rooms: { [key: string]: Room } = {};
+
+io.on("connection", (socket: Socket) => {
+  console.log(`${socket.id} connected`);
+
+  socket.on("joinRoom", (user: string, room: string) => {
+    if (rooms[room].players.length === 2) {
+      socket.emit("roomFull");
+    } else if (rooms[room].players.length === 1) {
+      io.emit("roomReady");
+    }
+    if (room !== "") {
+      socket.join(room);
+
+      rooms[room].players.push(user);
+      socket.data.room = room;
+      socket.data.user = user;
+
+      console.log(`User ${user} joined room ${room}`);
+      io.emit("availableRooms", { rooms });
+    }
+  });
+
+  socket.on("leaveRoom", (user: string, room: string) => {
+    socket.leave(room);
+    rooms[room].players = rooms[room].players.filter(player => player !== user)
+    if (rooms[room].players.length === 0) {
+      delete rooms[room];
+    }
+    console.log(`User ${user} left room`);
+    io.to(room).emit("opponentDisconnected");
+    io.emit("availableRooms", { rooms });
+  });
+
+  socket.on("createRoom", (user: string, room: string) => {
+    if (room !== "") {
+      socket.join(room);
+      console.log(`${user} created and joined room: ${room}`);
+      rooms[room] = {
+        players: [user],
+        rematchRequests: 0,
+        score: { player1: 0, player2: 0 },
+      };
+      console.log(rooms[room]);
+    }
+    socket.data.room = room;
+    socket.data.user = user;
+    io.emit("availableRooms", { rooms });
+  });
+
+  socket.on("getAvailableRooms", () => {
+    socket.emit("availableRooms", { rooms });
+  });
+
+  socket.on("groupFound", ( room: string, user: string, group: string ) => {
+    io.to(room).emit("groupFound", { user, group });
+  });
+
+  socket.on("incorrectGuess", ( room: string, user: string, group: string ) => {
+    io.to(room).emit("incorrectGuess", { user, group });
+  });
+
+  socket.on("gameOver", ( user: string, room: string, lives: number ) => {
+    if (rooms[room].players[0] === user) {
+      const increment1 = lives === 0 ? 0 : 1;
+      const increment2 = lives === 0 ? 1 : 0;
+      rooms[room].score = {
+        player1: rooms[room].score.player1 + increment1,
+        player2: rooms[room].score.player2 + increment2,
+      };
+    } else {
+      const increment1 = lives === 0 ? 0 : 1;
+      const increment2 = lives === 0 ? 1 : 0;
+      rooms[room].score = {
+        player1: rooms[room].score.player1 + increment2,
+        player2: rooms[room].score.player2 + increment1,
+      };
+    }
+    const scores = {
+      player1: rooms[room].score.player1,
+      player2: rooms[room].score.player2,
+    };
+    io.emit("checkScores", scores);
+  });
+
+  socket.on("rematchRequest", ( room: string ) => {
+    if (rooms[room]) {
+      rooms[room].rematchRequests += 1;
+      io.to(room).emit("rematchRequest");
+      checkRematchStatus(room);
+    }
+  });
+
+  socket.on("confirmRematch", ( room: string ) => {
+    if (rooms[room]) {
+      io.to(room).emit("confirmRematch");
+      resetRoomState(room);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+    const room = socket.data.room;
+    const user = socket.data.user;
+    if (room && rooms[room]) {
+        rooms[room].players = rooms[room].players.filter(player => player !== user)
+      if (rooms[room].players.length === 0) {
+        delete rooms[room];
+      }
+      console.log(
+        `User ${user} removed from room ${room} due to disconnection`
+      );
+      io.to(room).emit("opponentDisconnected");
+      io.emit("availableRooms", { rooms });
+    }
+  });
+
+  function checkRematchStatus(room: string) {
+    if (rooms[room] && rooms[room].rematchRequests === 2) {
+      io.to(room).emit("confirmRematch");
+      resetRoomState(room);
+    }
+  }
+
+  function resetRoomState(room: string) {
+    if (rooms[room]) {
+      rooms[room].rematchRequests = 0;
+    }
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
