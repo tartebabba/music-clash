6141b6ac8f76dfd35e7061619be046d89ebbed1a
Ted Smiles <ted@smilesfamily.co.uk>
2024-05-30 12:23:59 +0100

    small change

--- frontend/src/components/ProfilePage.tsx
+++ frontend/src/components/ProfilePage.tsx
@@ -1,19 +1,19 @@
 import { Image, Text, View } from 'react-native';
 import { Props } from './types';
 import { useUser } from './ContextProvider';
-import ProvideProfilePicture from '../utils/utils';
 
 export default function ProfilePage({ route }: Props) {
   const { user, setUser } = useUser();
   console.log(route.params);
   console.log(user, 'from profile page');
-  
+
   return (
-    <View
-      className='items-center p-5 pt-16'
-    >
-      <Image className='h-40 w-40 m-8 rounded-full' source={require('../../../assets/default_pfp.jpeg')} />
-      <Text className='text-4xl '>{user?.name}</Text>
+    <View className="items-center p-5 pt-16">
+      <Image
+        className="h-40 w-40 m-8 rounded-full"
+        source={require('../../../assets/default_pfp.jpeg')}
+      />
+      <Text className="text-4xl ">{user?.name}</Text>
       <Text>{user?.username}</Text>
       <Text>{user?.email}</Text>
     </View>
